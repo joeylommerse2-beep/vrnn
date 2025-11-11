183eb7a884c7eba3cda493af580237d2ff89bda6
Joey Lommerse <joeylommerse2@gmail.com>
2025-11-11 10:37:53 +0100

    Save local changes before pull

--- Loss_Function_LFADS.py
+++ Loss_Function_LFADS.py
@@ -6,7 +6,7 @@ Created on Sat Nov  8 10:06:39 2025
 """
 
 def poisson_loss(rates, x):
-    return (rates - x * torch.log(rates + 1e-8)).mean()
+    return (rates - x * torch.log(rates + 1e-8)).sum(dim=(1,2)).mean()
 
 
 def lfads_loss(rates, x, kl_ic, kl_ctrl, kl_weight):
